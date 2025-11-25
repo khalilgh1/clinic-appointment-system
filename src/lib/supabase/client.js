e622349f0e764b15185fa5d37aab22612dfe45a1
KhalilGH <khalil.ghanem@ensia.edu.dz>
2025-11-25 23:00:44 +0100

    updated the client name to supabase for ez use

--- src/lib/supabase/client.js
+++ src/lib/supabase/client.js
@@ -1,7 +1,7 @@
 //supabase initialization here
 import { createBrowserClient } from '@supabase/ssr'
 
-export function createClient() {
+export function supabase() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
