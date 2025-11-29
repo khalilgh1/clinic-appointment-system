95e7c782107be467fc622a44d5939640274328d5
bachounda Essedik <gamerseddik@gmail.com>
2025-11-29 20:57:49 +0100

    cleaning code

--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -4,7 +4,7 @@ import { createClient } from "@/lib/supabase/server";
 
 
 export default async function Page({ params }) {
-  // ⬅️ FIX: params is a Promise
+  
   const resolved = await params;
   const numericId = Number(resolved.id);
 
