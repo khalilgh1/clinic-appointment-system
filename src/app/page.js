cb4e18358ddaa07a05328c63d86a99b8571e9509
KhalilGH <khalil.ghanem@ensia.edu.dz>
2025-11-30 09:19:36 +0100

    fixed the naming the home placement issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 //import MedicalBookingPage from "./(public)/(appoint)/appoint_sidebar";
 import MedicalBookingPage from "@/app/(public)/MedicalBooking/page";
-import HomePage from "./(public)/page";
+import HomePage from "./(public)/HomePage";
 export default function Home() {
   return (
     <div className="min-h-screen w-full">
